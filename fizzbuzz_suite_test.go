333e3b7387c37ea76365460052525e9172bb34de
Stuart Warren <stuartwarren@ntlworld.com>
2016-03-25 17:08:41 +0000

    add junit reporter

--- fizzbuzz_suite_test.go
+++ fizzbuzz_suite_test.go
@@ -3,11 +3,12 @@ package fizzbuzz_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
+	"github.com/onsi/ginkgo/reporters"
 	"testing"
 )
 
 func TestFizzbuzz(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Fizzbuzz Suite")
+	junitReporter := reporters.NewJUnitReporter("junit.xml")
+	RunSpecsWithDefaultAndCustomReporters(t, "Fizzbuzz Suite", []Reporter{junitReporter})
 }
