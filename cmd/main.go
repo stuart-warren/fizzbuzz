a8874bf4b2c619b2dea7acf334cebf64a490abb4
Stuart Warren <stuartwarren@ntlworld.com>
2018-03-24 14:02:25 +0000

    doc: Add FizzBuzz function comment

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,11 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"github.com/stuart-warren/fizzbuzz"
 	"io"
 	"log"
 	"os"
+
+	"github.com/stuart-warren/fizzbuzz"
 )
 
 // Run runs the command and returns nil if successful, otherwise
@@ -16,15 +17,10 @@ func Run(in io.Reader, out io.Writer, args []string) error {
 	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
 	flags.Parse(args[1:])
 	_ = bufio.NewScanner(in)
-	//for s.Scan() {
-	//out.Write(s.Bytes())
+
 	for i := 0; i < 100; i++ {
 		io.WriteString(out, fmt.Sprintf("%s\n", fizzbuzz.FizzBuzz(i)))
 	}
-	//}
-	//if err := s.Err(); err != nil {
-	//	return err
-	//}
 
 	return nil
 }
--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,6 +2,10 @@ package fizzbuzz
 
 import "strconv"
 
+// FizzBuzz takes an input integer and if
+// that divides by 3 returns 'fizz', by 5
+// returns 'buzz', by both 3 and 5 returns
+// 'fizzbuzz', otherwise the input number.
 func FizzBuzz(input int) string {
 	if input%5 == 0 && input%3 == 0 {
 		return "fizzbuzz"
